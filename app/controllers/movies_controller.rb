de88401889630b7dddce44cb4f76ea88d9814851
Sleipnir2000 <jionghua_chen@ucsb.edu>
2023-01-30 17:52:31 -0800

    Add Byebug

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,20 @@ class MoviesController < ApplicationController
     end
 
     def new
-        @movie = Movie.new 
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.create movie_params
+        if @movie
+            redirect_to movies_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+        
+    private 
+    def movie_params 
+        params.require(:movie).permit(:title, :rating, :released_date)
     end
 end
